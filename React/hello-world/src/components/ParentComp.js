82431c8aad00cc0fa26049303637266f09b032cf
Orvin Jed Alava <orvinjedalavacertify@gmail.com>
2024-06-27 09:07:28 +1000

    React.memo - MemoComp.js

--- React/hello-world/src/components/ParentComp.js
+++ React/hello-world/src/components/ParentComp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RegComp from './RegComp.js';
 import PureComp from './PureComp.js';
+import MemoComp from './MemoComp.js';
 
 class ParentComp extends Component {
     constructor(props) {
@@ -24,8 +25,9 @@ class ParentComp extends Component {
         return (
             <div>
                 Parent Component
-                <RegComp name={this.state.name}></RegComp>
-                <PureComp name={this.state.name}></PureComp>
+                <MemoComp name={this.state.name}/>
+                {/*<RegComp name={this.state.name}></RegComp>*/}
+                {/*<PureComp name={this.state.name}></PureComp>*/}
             </div>
         );
     }
