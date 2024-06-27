16125b3f655302f0a9c1cf6358e919fc2b5f7266
Orvin Jed Alava <orvinjedalavacertify@gmail.com>
2024-06-27 17:01:37 +1000

    Hooks basic

--- TodoApi/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172d76bf6ed3754e31e1ed77819944e51f04068e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ccb9b0700b12d163a11f829c95027839cc1edfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
