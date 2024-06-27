f38f2f8b72bd7f279e73b94d788419f410239cca
Orvin Jed Alava <orvinjedalavacertify@gmail.com>
2024-06-27 17:18:12 +1000

    prevCount

--- React/state-hook/src/App.js
+++ React/state-hook/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter.js';
 import HookCounter from './components/HookCounter.js';
+import HookCounterTwo from './components/HookCounterTwo.js';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter />
+      <HookCounterTwo />
+      {/*<HookCounter />*/}
       {/*<ClassCounter />*/}
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
