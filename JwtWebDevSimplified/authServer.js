878c5d746cc453e3011d9f442dab577c8d9faea2
Orvin Jed Alava <orvinjedalavacertify@gmail.com>
2024-07-02 15:20:44 +1000

    Updates

---
+++ JwtWebDevSimplified/authServer.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+    // Authenticate User
+
+    const username = req.body.username;
+    const user = { name: username };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+    res.json({ accessToken: accessToken });
+});
+
+app.listen(4000);
