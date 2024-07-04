a3990d669cdc056396eab5723441c12a48dceed6
Orvin Jed Alava <orvinjedalavacertify@gmail.com>
2024-07-04 14:00:24 +1000

    InitialCreate

---
+++ RunGroopWebApp/RunGroopWebApp/Data/Enums/ClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroopWebApp.Data.Enums
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
---
+++ RunGroopWebApp/RunGroopWebApp/Data/Enums/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroopWebApp.Data.Enums
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        FiveK,
+        TenK,
+        HalfMarathon
+    }
+}
