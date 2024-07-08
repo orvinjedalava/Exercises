b148b4dadec356c6110b47205eb140366237ec0e
Orvin Jed Alava <orvinjedalavacertify@gmail.com>
2024-07-09 09:44:27 +1000

    HookMouse Updates

--- React/effect-hook/src/App.js
+++ React/effect-hook/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import ClassCounterOne from './components/ClassCounterOne.js';
 import HookCounterOne from './components/HookCounterOne.js';
+import ClassMouse from './components/ClassMouse.js';
+import HookMouse from './components/HookMouse.js';
 
 function App() {
   return (
     <div className="App">
       {/*<ClassCounterOne />*/}
-      <HookCounterOne />
+      {/*<HookCounterOne />*/}
+      {/*<ClassMouse />*/}
+      <HookMouse />
     </div>
   );
 }
