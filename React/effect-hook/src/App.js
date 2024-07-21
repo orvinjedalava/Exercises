5c16fe934fc104cd7c5a82dda95810148b5cc8a4
Orvin Jed Alava <orvinjedalavacertify@gmail.com>
2024-07-22 09:02:26 +1000

    updates

--- React/effect-hook/src/App.js
+++ React/effect-hook/src/App.js
@@ -8,10 +8,20 @@ import MouseContainer from './components/MouseContainer.js';
 import IntervalClassCounter from './components/IntervalClassCounter.js';
 import IntervalHookCounter from './components/IntervalHookCounter.js';
 import DataFetching from './components/DataFetching.js';
+import ComponentC from './components/ComponentC.js';
+import React from 'react';
+
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
+      <UserContext.Provider value={'Jed'}>
+        <ChannelContext.Provider value={'Codevolution'}>
+          <ComponentC/>
+        </ChannelContext.Provider>
+      </UserContext.Provider>
       {/*<ClassCounterOne />*/}
       {/*<HookCounterOne />*/}
       {/*<ClassMouse />*/}
@@ -19,7 +29,7 @@ function App() {
       {/*<MouseContainer/>*/}
       {/*<IntervalClassCounter />*/}
       {/*<IntervalHookCounter />*/}
-      <DataFetching/>
+      {/*<DataFetching/>*/}
     </div>
   );
 }
---
+++ React/reducer-hook/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterOne from './components/CounterOne.js';
+import CounterTwo from './components/CounterTwo.js';
+
+function App() {
+  return (
+    <div className="App">
+      {/*<CounterOne/>*/}
+      <CounterTwo/>
+    </div>
+  );
+}
+
+export default App;
