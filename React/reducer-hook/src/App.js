e987be99f271c8c65050f3793871ae56c8f9b267
Orvin Jed Alava <orvinjedalavacertify@gmail.com>
2024-07-22 14:01:04 +1000

    Updates

--- React/reducer-hook/src/App.js
+++ React/reducer-hook/src/App.js
@@ -2,13 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import CounterOne from './components/CounterOne.js';
 import CounterTwo from './components/CounterTwo.js';
+import CounterThree from './components/CounterThree.js';
+import ComponentA from './components/ComponentA.js';
+import ComponentB from './components/ComponentB.js';
+import ComponentC from './components/ComponentB.js';
+import React, { useReducer } from 'react';
+
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch(action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      {/*<CounterOne/>*/}
-      <CounterTwo/>
-    </div>
+    <CountContext.Provider value={{ countState: count, countDispatch: dispatch }}>
+      <div className="App">
+        Count - {count}
+        {/*<CounterOne/>*/}
+        {/*<CounterTwo/>*/}
+        {/*<CounterThree/>*/}
+        <ComponentA/>
+        <ComponentB/>
+        <ComponentC/>
+      </div>
+    </CountContext.Provider>
   );
 }
 
