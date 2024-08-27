3a2029dcdcd47b1172fd053f417a8b540839855b
orvinjedalava <ojedbub@gmail.com>
2024-08-28 09:46:28 +1000

    Adding files for Tutorial 21 - Template Literals and Exclude

--- ReactTypeScript/react-typescript-demo/src/App.tsx
+++ ReactTypeScript/react-typescript-demo/src/App.tsx
@@ -26,6 +26,7 @@ import { Profile } from './components/auth/Profile';
 import { Private } from './components/auth/Private';
 import { List } from './components/generics/List';
 import { RandomNumber } from './components/restriction/RandomNumber';
+import { Toast } from './components/templateliterals/Toast';
 
 function App() {
   // const personName = {
@@ -120,7 +121,8 @@ function App() {
         onClick={(item) => {
           console.log(item)}}
       /> */}
-      <RandomNumber value={10} isPositive/>
+      {/* <RandomNumber value={10} isPositive/> */}
+      <Toast position='center'/>
 
     </div>
   );
