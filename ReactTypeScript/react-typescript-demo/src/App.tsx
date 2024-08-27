a55c22f6f720f1074161dbcaf72828be0ec73e46
orvinjedalava <ojedbub@gmail.com>
2024-08-27 10:06:08 +1000

    Adding files for Tutorial 5 - Advanced Props

--- ReactTypeScript/react-typescript-demo/src/App.tsx
+++ ReactTypeScript/react-typescript-demo/src/App.tsx
@@ -3,32 +3,41 @@ import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne'
-  }
+  // const personName = {
+  //   first: 'Bruce',
+  //   last: 'Wayne'
+  // }
 
-  const nameList = [
-      {
-        first: 'Bruce',
-        last: 'Wayne'
-      },
-      {
-        first: 'Clark',
-        last: 'Kent'
-      },
-      {
-        first: 'Princess',
-        last: 'Diana'
-      }
-    ]
+  // const nameList = [
+  //   {
+  //     first: 'Bruce',
+  //     last: 'Wayne'
+  //   },
+  //   {
+  //     first: 'Clark',
+  //     last: 'Kent'
+  //   },
+  //   {
+  //     first: 'Princess',
+  //     last: 'Diana'
+  //   }
+  // ]
   return (
     <div className="App">
-        <Greet name='Jed' messageCount={20} isLoggedIn={false} />
+        {/* <Greet name='Jed' messageCount={20} isLoggedIn={false} />
         <Person name={personName}/>
-        <PersonList names={nameList}/>
+        <PersonList names={nameList}/> */}
+        <Status status='loading'/>
+        <Heading>Placeholder text</Heading>
+        <Oscar>
+          <Heading>Oscar goes to Leonardo Dicaprio</Heading>
+        </Oscar>
+        <Greet name='Jed' isLoggedIn={true} />
     </div>
   );
 }
