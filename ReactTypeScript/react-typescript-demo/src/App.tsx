450e2120d3e9d6d804c716836f1ed60ddf68427b
orvinjedalava <ojedbub@gmail.com>
2024-08-27 14:01:09 +1000

    Adding files for Tutorial 10 - useState Future Value

--- ReactTypeScript/react-typescript-demo/src/App.tsx
+++ ReactTypeScript/react-typescript-demo/src/App.tsx
@@ -11,6 +11,7 @@ import { Button } from './components/Button';
 import { Input } from './components/Input';
 import { Container } from './components/Container';
 import { LoggedIn } from './components/state/LoggedIn';
+import { User } from './components/state/User';
 
 function App() {
   // const personName = {
@@ -54,6 +55,7 @@ function App() {
         }}/> */}
         {/* <Container styles={{border: '1px solid black', padding: '1rem'}}/> */}
         <LoggedIn />
+        <User />
     </div>
   );
 }
