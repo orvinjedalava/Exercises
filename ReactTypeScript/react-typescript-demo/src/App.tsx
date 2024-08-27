7c9aa2a03dc04b84ef0f0cacdbd9a8ee9ca034cc
orvinjedalava <ojedbub@gmail.com>
2024-08-27 10:55:23 +1000

    Adding files from Tutorial - 6 - Event Props

--- ReactTypeScript/react-typescript-demo/src/App.tsx
+++ ReactTypeScript/react-typescript-demo/src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { lstat } from 'fs';
 import './App.css';
 import { Greet } from './components/Greet';
@@ -6,6 +7,8 @@ import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
 import { Heading } from './components/Heading';
 import { Oscar } from './components/Oscar';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
   // const personName = {
@@ -27,17 +30,26 @@ function App() {
   //     last: 'Diana'
   //   }
   // ]
+  const [value, setValue]= useState('');
+
   return (
     <div className="App">
         {/* <Greet name='Jed' messageCount={20} isLoggedIn={false} />
         <Person name={personName}/>
         <PersonList names={nameList}/> */}
-        <Status status='loading'/>
+        {/* <Status status='loading'/>
         <Heading>Placeholder text</Heading>
         <Oscar>
           <Heading>Oscar goes to Leonardo Dicaprio</Heading>
         </Oscar>
-        <Greet name='Jed' isLoggedIn={true} />
+        <Greet name='Jed' isLoggedIn={true} /> */}
+        <Button handleClick={(event, id) => {
+          console.log('Button clicked', event, id)
+        }}/>
+        <Input value={value} handleChange={(event) => {
+          console.log(event.target.value)
+          setValue(event.target.value)
+        }}/>
     </div>
   );
 }
